35c5e823da03bfe7bf94fd4ee871ced04d8be5d3
Mateusz <mateusz.rudnicki05@wp.pl>
2026-04-08 13:38:58 +0200

    Add custom GymUser model and format code with black

--- workouts/models.py
+++ workouts/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class GymUser(AbstractUser):
+    years_of_experience = models.PositiveIntegerField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Gym User"
+        verbose_name_plural = "Gym Users"
+
+    def __str__(self):
+        return self.username
