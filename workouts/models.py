0b3ea13b63bccde415350bee786c63023f92f0d2
Mateusz <mateusz.rudnicki05@wp.pl>
2026-04-08 14:14:45 +0200

    Add Workout model with user relationship

--- workouts/models.py
+++ workouts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 
 class GymUser(AbstractUser):
@@ -33,3 +34,16 @@ class Exercise(models.Model):
     def __str__(self):
         return self.name
 
+
+class Workout(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=256)
+    description = models.TextField(blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Workout"
+        verbose_name_plural = "Workouts"
