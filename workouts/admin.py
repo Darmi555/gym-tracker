203b9797b184cb7542f7c8e729f54d6446ba92fd
Mateusz <mateusz.rudnicki05@wp.pl>
2026-04-08 18:08:11 +0200

    feat: register Category and Exercise models in admin

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from workouts.models import Category, Exercise
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name",)
+
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "get_category", "description",)
+
+    def get_category(self, obj):
+        return ", ".join([cat.name for cat in obj.categories.all()])
+    get_category.short_description = "Category"
