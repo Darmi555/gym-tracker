91344294115b0d1c897d0444ef6e37b4dface5e4
Mateusz <mateusz.rudnicki05@wp.pl>
2026-04-08 14:00:26 +0200

    Add Category and Exercise models

--- workouts/models.py
+++ workouts/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# Create your models here.
 class GymUser(AbstractUser):
     years_of_experience = models.PositiveIntegerField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
@@ -13,3 +12,24 @@ class GymUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(blank=True, null=True)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
